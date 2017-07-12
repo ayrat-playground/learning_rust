2cdf7a68e0950787832215493f78d81d139873bc
Ayrat Badykov <ayratin555@gmail.com>
2017-07-12 08:08:57 +0300

    add mut, shodowng examples

---
+++ variables/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let mut x = 1;
+    println!("The value of x is: {}", x);
+
+    x = 6;
+    println!("The value of x is: {}", x);
+
+    let y = 1;
+
+    let y = 5 + y;
+
+    let y = y * 2;
+
+    println!("The value of y is: {}", y);
+}
