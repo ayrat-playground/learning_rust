32ea4f3d425d25b8b9aa69cebea92f564d35c4f6
Ayrat Badykov <ayratin555@gmail.com>
2017-07-26 22:29:39 +0300

    add some borrowing examples

---
+++ borrowing/src/main.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let s1 = String::from("hello");
+    let len = calculate_length(&s1);
+    println!("The length of '{}' is '{}'", s1, len);
+
+    let mut s2 = String::from("hello");
+    change(&mut s2);
+    println!("Changes string: {}", s2);
+
+    let first_word = first_word(&s2);
+    println!("First word is {}", first_word);
+    s2.clear();
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+fn first_word(s: &String) -> usize {
+    let bytes = s.as_bytes();
+
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
+}
