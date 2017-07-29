60bb82c4f55f864fefbd508b710deb9a6f75bd66
Ayrat Badykov <ayratin555@gmail.com>
2017-07-29 16:12:31 +0300

    add more struct examples

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -6,6 +6,33 @@ struct User {
 }
 struct Color(i32, i32, i32);
 
+#[derive(Debug)]
+struct Rectangle {
+    length: u32,
+    width: u32
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle { length: size, width: size }
+    }
+}
+
+struct AnotherRectangle {
+    length: u32,
+    width: u32
+}
+
+impl AnotherRectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
+
+    fn can_hold(&self, rectangle: &AnotherRectangle) -> bool {
+        (self.length >= rectangle.length) && (self.width >= rectangle.width)
+    }
+}
+
 fn main() {
     let user = build_user(String::from("email"), String::from("die_on_time"));
     println!("User username is {}", user.username);
@@ -16,6 +43,19 @@ fn main() {
     let Color(a, b, c) = Color(5, 6, 7);
     println!("a is {}", a);
 
+    let rectangle = Rectangle { width: 10, length: 20 };
+    let area = area(&rectangle);
+    println!("area is {}", area);
+    println!("rectangle is {:#?}", rectangle);
+
+    let another_rectangle = AnotherRectangle { width: 11, length: 21 };
+    println!("area of anotherRectangle is {}", another_rectangle.area());
+
+    let another_rectangle1 = AnotherRectangle { width: 9, length: 19 };
+    println!("another_rectangle can hold? another_rectangle1: {}", another_rectangle.can_hold(&another_rectangle1));
+
+    let square = Rectangle::square(10);
+    println!("square is {:?}", square);
 }
 
 fn build_user(email: String, username: String) -> User {
@@ -32,4 +72,8 @@ fn change_username(user: User, username: String) -> User {
         username,
         ..user
     }
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.length * rectangle.width
 }
